b8b802612b59689359ba7e0f5eaed08046c9136f
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-07-24 17:11:09 +0200

    Add files via upload

--- src/headers/pool.h
+++ src/headers/pool.h
@@ -1,4 +1,21 @@
 #ifndef THREADPOOL
 	#define THREADPOOL
 	
+	typedef struct ThreadPool ThreadPool;
+	
+	//crea il threadpool
+	ThreadPool *create_pool(unsigned int);
+	
+	//attende che il threadpool finisca di elaborare i task passati
+	void await_completion(ThreadPool*);
+	
+	//distrugge il threadpool
+	void destroy_pool(Threadpool*);
+	
+	//Inserisce task in coda
+	void enqueue_task(Threadpool*,char*);
+	
+	void add_worker(Threadpool*);
+	void remove_worker(Threadpool*);
+	
 #endif
