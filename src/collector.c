2b6eb395fc9108e105a205dce91e4d46afe914c9
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-08-15 11:59:03 -0400

    Add files via upload

--- src/collector.c
+++ src/collector.c
@@ -1,9 +1,20 @@
 /*
 Questa sezione di codice contiene la parte del processo Collector
+
+TODO timer ogni secondo per la stampa dei risultati parziali
 */
 
 #include <pthread.h>
 #include <stdio.h>
+
+#include "utils.h"
+
+//tipo dei risultati da stampare
+//andrebbe messo in un unico header a cui fa riferimento anche workfun/pool
+typedef struct result_path {
+	long result;
+	char *pathname;
+} result_path_t;
 
 int main(int argc, char *argv[]) {
 	fprintf(stdout,"---Collector Parte---\n");
--- src/workfun.c
+++ src/workfun.c
@@ -4,5 +4,11 @@ Autore: Baldini Enrico
 
 -----------
 
-Questa sezione di codice contiene la funzione lanciata dai worker thread del pool
+Questa sezione di codice contiene la funzione usata dai worker thread per calcolare il valore di un dato file.
+Il risultato atteso e' la sommatoria "pesata" che va da 0 a N-1, dove N e' il numero di interi long nel file, del
+prodotto tra 'i' e l'i-esimo intero.
 */
+
+int workfun(char *filename){
+	
+}
