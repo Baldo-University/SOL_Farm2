dabee41e8e6d87cdf82042ea05cb10f9d7b7a6a1
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-08-21 13:09:54 -0400

    Add files via upload

--- src/headers/pool.h
+++ src/headers/pool.h
@@ -1,16 +1,26 @@
 #ifndef THREADPOOL
 	#define THREADPOOL
 	
+	//lunghezza max di stringa aumentata di 1 per accomodare il carattere di terminazione
+	#define MAX_PATHNAME_LEN 256
+
 	//linked list di thread worker
 	typedef struct workerlist {
 		pthread_t worker;
 		struct workerlist *next;
 	} workerlist_t;
 	
+	//linked list di task
+	typedef struct tasklist {
+		char task[MAX_PATHNAME_LEN];
+		struct tasklist *next;
+	} tasklist_t;
+	
 	//Struttura principale del threadpool
 	typedef struct threadpool {
 		volatile unsigned short initialized;	//coda inizializzata (per attesa attiva che i thread siano pronti)
 		_Atomic unsigned short running;			//coda attiva o no
+		char *socket;							//socket a cui si devono collegare i worker
 		workerlist *worker_head;				//puntatore di testa alla lista dei worker
 		workerlist *worker_tail;				//puntatore di coda alla suddetta lista
 		unsigned int num_threads;				//numero totale di worker, minimo 1
@@ -19,13 +29,12 @@
 		unsigned int threadID;					//singolo identificatore dei thread
 		pthread_mutex_t worker_mtx;				//worker attivo o idling
 		pthread_cond_t worker_cond;				//condizione relativa al suddetto mutex
-		char **tasks;							//coda di produzione di tipo circolare
+		tasklist_t *tasks_head;					//coda di produzione
+		tasklist_t *tasks_tail;					//ultimo elemento della coda
 		size_t queue_size;						//lunghezza della coda
-		int tasks_head;							//indice task in testa
-		int tasks_tail;							//indice task in coda
+		size_t cur_queue_size;					//dimensione corrente della coda 
 		pthread_mutex_t task_mtx;				//mutex coda task
-		pthread_cond_t task_full;				//condizione coda piena
-		char *socket;							//socket a cui si devono collegare i worker
+		pthread_cond_t task_cond;				//condizione di spazio presente in coda
 	} threadpool_t;
 
 	//inizializzazione threadpool
@@ -33,18 +42,18 @@
 	threadpool_t initialize_pool(long, size_t, char*);
 	
 	//attende che il threadpool finisca di elaborare i task passati
-	void await_pool_completion();
+	void await_pool_completion(threadpool_t*);
 	
 	//distrugge il threadpool
-	void destroy_pool();
+	void destroy_pool(threadpool_t*);
 	
 	//Inserisce task in coda. Usato da masterworker
-	void enqueue_task(char*);
+	void enqueue_task(threadpool_t*,char*);
 	
 	//Aggiunge thread worker
-	int add_workers(threadpool_t*,long);
+	int add_workers(threadpool_t*,unsigned int);
 	
 	//Rimuove thread worker
-	void remove_worker();
+	void remove_worker(threadpool_t*);
 	
 #endif
