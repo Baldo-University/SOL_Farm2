13b193f4a098ee4387d70d4dc5d3c748683ff832
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-08-24 10:56:57 -0400

    Add files via upload

--- src/headers/pool.h
+++ src/headers/pool.h
@@ -2,11 +2,34 @@
 	#define THREADPOOL
 	
 	//Struttura principale del threadpool
-	typedef struct threadpool threadpool_t;
+	
+	//linked list di thread worker
+	typedef struct workerlist workerlist_t;
+		
+	//linked list di task
+	typedef struct tasklist tasklist_t;
+
+	typedef struct threadpool {	//struct threadpool;
+		_Atomic unsigned short running;		//coda attiva o no
+		char *socket;						//socket a cui si devono collegare i worker
+		workerlist_t *worker_head;			//puntatore di testa alla lista dei worker
+		workerlist_t *worker_tail;			//puntatore di coda alla suddetta lista
+		unsigned int num_threads;			//numero totale di worker, minimo 1
+		unsigned int remove_threads;		//worker da rimuovere
+		unsigned int threadID;				//singolo identificatore dei thread
+		pthread_mutex_t worker_mtx;			//mutex della lista di worker
+		pthread_cond_t worker_cond;			//condizione relativa alla presenza di task in coda
+		tasklist_t *tasks_head;				//coda di produzione
+		tasklist_t *tasks_tail;				//ultimo elemento della coda
+		size_t queue_size;					//lunghezza della coda
+		size_t cur_queue_size;				//dimensione corrente della coda 
+		pthread_mutex_t task_mtx;			//mutex coda task
+		pthread_cond_t task_cond;			//condizione di spazio presente in coda
+	} threadpool_t;
 
 	//inizializzazione threadpool
 	//param: numero di worker iniziali, lunghezza coda, socket
-	threadpool_t initialize_pool(long, size_t, char*);
+	threadpool_t *initialize_pool(long, size_t, char*);
 	
 	//attende che il threadpool finisca di elaborare i task passati
 	int await_pool_completion(threadpool_t*);
