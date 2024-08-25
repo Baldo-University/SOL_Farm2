bbeebeffe02cd2bf39564aa747ffa7b92ce6d1bc
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-08-25 13:12:10 -0400

    Add files via upload

--- src/headers/pool.h
+++ src/headers/pool.h
@@ -15,16 +15,18 @@
 		workerlist_t *worker_head;			//puntatore di testa alla lista dei worker
 		workerlist_t *worker_tail;			//puntatore di coda alla suddetta lista
 		unsigned int num_threads;			//numero totale di worker, minimo 1
+		unsigned int started_threads;		//thread inizializzati e pronti ad eseguire task
 		unsigned int remove_threads;		//worker da rimuovere
 		unsigned int threadID;				//singolo identificatore dei thread
 		pthread_mutex_t worker_mtx;			//mutex della lista di worker
-		pthread_cond_t worker_cond;			//condizione relativa alla presenza di task in coda
+		pthread_cond_t worker_cond;			//condizione usata per verificare se un worker giri o meno
 		tasklist_t *tasks_head;				//coda di produzione
 		tasklist_t *tasks_tail;				//ultimo elemento della coda
 		size_t queue_size;					//lunghezza della coda
 		size_t cur_queue_size;				//dimensione corrente della coda 
 		pthread_mutex_t task_mtx;			//mutex coda task
-		pthread_cond_t task_cond;			//condizione di spazio presente in coda
+		pthread_cond_t empty_queue_cond;	//condizione per attesa che coda non sia piu' vuota
+		pthread_cond_t full_queue_cond;		//condizione per attesa che coda non sia piu' piena
 	} threadpool_t;
 
 	//inizializzazione threadpool
@@ -35,7 +37,7 @@
 	int await_pool_completion(threadpool_t*);
 	
 	//Inserisce task in coda. Usato da masterworker
-	void enqueue_task(threadpool_t*,char*);
+	int enqueue_task(threadpool_t*,char*);
 	
 	//Aggiunge un thread worker
 	void add_worker(threadpool_t*);
