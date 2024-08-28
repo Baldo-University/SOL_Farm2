1f4ac94dee769fbe4defcfb67cf3db6b8e1aee7b
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-08-28 13:13:07 -0400

    Add files via upload

---
+++ src/headers/message.h
@@ -0,0 +1,12 @@
+#ifndef MESSAGE
+	#define MESSAGE
+	
+	#define MAX_PATHNAME_LEN 256
+	
+	//struct del tipo di messaggio che un worker deve inviare al collector
+	typedef struct result_path {
+		long result;
+		char pathname[MAX_PATHNAME_LEN];
+	} result_path_t;
+	
+#endif MESSAGE
