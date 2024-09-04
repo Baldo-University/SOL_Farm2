ccf1a15c589209aa17c60fd95f5bc0b154e9da2d
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-09-04 12:33:32 -0400

    Add files via upload

--- src/headers/message.h
+++ src/headers/message.h
@@ -4,6 +4,9 @@
 	#include "utils.h"
 	
 	#define MAX_PATHNAME_LEN 1+MAX_NAMELENGTH
+	#define UNIX_PATH_MAX 108	//lunghezza massima pathname socket
+	#define BUFFER_SIZE 264		//dimensione del buffer di invio dati
+	#define DISCONNECT "close"	//messaggio di disconnessione
 	
 	typedef struct message {
 		char name[MAX_PATHNAME_LEN];
