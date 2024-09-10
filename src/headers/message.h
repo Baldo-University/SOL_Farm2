354fc9b62e87dd9063f2f37735117d25b8dd5bf3
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-09-10 10:26:01 -0400

    Add files via upload

--- src/headers/message.h
+++ src/headers/message.h
@@ -3,7 +3,10 @@
 	
 	#include "utils.h"
 	
-	#define MAX_PATHNAME_LEN 1+MAX_NAMELENGTH
+	#ifndef MAX_PATHNAME_LEN
+		#define MAX_PATHNAME_LEN 1+MAX_NAMELENGTH
+	#endif
+	
 	#define UNIX_PATH_MAX 108	//lunghezza massima pathname socket
 	#define BUFFER_SIZE 264		//dimensione del buffer di invio dati
 	#define DISCONNECT "close"	//messaggio di disconnessione
