985009154d35ff795d2fafc38d854f264e051689
Baldo-University <73953828+Baldo-University@users.noreply.github.com>
2024-09-11 12:18:12 -0400

    Add files via upload

--- collt.c
+++ collt.c
@@ -1,7 +1,15 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
 
 #include "message.h"
+#include "utils.h"
+
+#define SOCKNAME ".littlefarm.sck"
 
 long workfun(char *filename) {
 	long result=0;
@@ -21,4 +29,50 @@ long workfun(char *filename) {
 	if(fclose(task)!=0)
 		perror("workfun, fclose");
 	return result;
+}
+
+int main (void) {
+	int fd_skt;
+	struct sockaddr_un sa;
+	int already_written=0, just_written=0, to_write=0; 
+	
+	pid_t collector=fork();
+	switch(collector) {
+	case -1:	//errore fork
+		perror("Main, fork");
+		exit(EXIT_FAILURE);
+	case 0:		//figlio
+		execl("./collector","collector",SOCKNAME,(char*)NULL);
+		//se si arriva a questa sezione di codice qualcosa e' andato storto
+		perror("Main, execl collector");
+		exit(EXIT_FAILURE);
+	default:	//padre
+		strncpy(sa.sun_path,SOCKNAME,UNIX_PATH_MAX);
+		sa.sun_family=AF_UNIX;
+		ec_is(fd_skt=socket(AF_UNIX,SOCK_STREAM,0),-1,"Client, socket");
+		while(connect(fd_skt,(struct sockaddr*)&sa,sizeof(sa))==-1) {
+			if(errno==ENOENT) {
+				fprintf(stderr,"Client, ENOENT\n");
+				sleep(1);
+			}
+			else {
+				perror("client, connect");
+				exit(EXIT_FAILURE);
+			}
+		}
+		fprintf(stderr,"Client: connesso al collector\n");
+		
+		already_written=0;
+		to_write=
+		
+		
+	}
+	
+	int collector_status;
+	collector=waitpid(collector,&collector_status,0);
+	if(WIFEXITED(collector_status))
+		fprintf(stderr,"Main: stato collector: %d\n",WEXITSTATUS(collector_status));
+	
+	fprintf(stderr,"Fine programma\n");
+	return 0;
 }
